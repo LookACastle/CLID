768011b4c2c6dca7d88b0a6c70e4ba8a310c14f5
LookACastle <thorkik@gmail.com>
2018-04-06 20:20:42 +0200

    Added chat functionality, for now the channel is hardcoded

--- client_file.py
+++ client_file.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import discord
 import asyncio, sys
-import discord
+import aioconsole
 
 class client(discord.Client):
 	def __init__(self, token, *args, **kwargs):
@@ -26,31 +26,43 @@ class client(discord.Client):
 	def run(self):
 		super().run()
 	
-	
 	async def send_message(self, channel_name, message):
 		
-		#Hopefully very temporary code
+		#Hopefully very temporary code, reason being that if multiple channels share a name this is positive for both.
 		for channel in self.get_all_channels():
 			if channel.name == channel_name:
 				channel_object = channel
 	
 		await super().send_message(channel_object, content=message)
 
-	async def on_typing(self, channel, user, when):
-		if channel.name == "testserv":
-			await self.send_message("testserv", "testy testing from testland")
+	async def async_input(self):
+		input = await aioconsole.ainput(">>> ")
+		return input
+		
+	async def on_message(self, message):
+		if message.author != self.user:
+			print(message.author.name + ": " + message.content)
+		#TODO: make this not break the layout
+
+	#async def on_typing(self, channel, user, when):
+	#	if channel.name == "testserv":
+	#		await self.send_message("testserv", "testy testing from testland")
 		
 	async def on_ready(self):
-		def get_servers():
-			servers = []
-			for text_channel in self.get_all_channels():
-				if text_channel.server not in servers:
-					#print(str(text_channel) + "\n" + str(text_channel.server) + "\n" + str(text_channel) + "\n" + str(servers))
-					servers.append(text_channel.server)
-			return servers
+	#	def get_servers():
+	#		servers = []
+	#		for text_channel in self.get_all_channels():
+	#			if text_channel.server not in servers:
+	#				servers.append(text_channel.server)
+	#		return servers
 		
-		servers = get_servers()
-		for server in servers:
-			print(server.name)
+	#	servers = get_servers()
+	#	for server in servers:
+	#		print(server.name)
 		
-	
+		while True:
+			try:
+				await self.send_message("testserv", await self.async_input())
+			except: 
+				pass
+
